dd0a60879361201ce97a0ab65e4bfc3a2136e6c2
PRATYUSH KUMAR <pratyush1002@gmail.com>
2024-07-19 12:07:04 +0530

    Download Project from Spring Initializr

---
+++ src/main/java/com/pratyush/springcouchbase/SpringBootWithCouchbaseApplication.java
@@ -0,0 +1,13 @@
+package com.pratyush.springcouchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithCouchbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithCouchbaseApplication.class, args);
+	}
+
+}
