f481e8530d74ac4d7a4969b77386b4d6d811aa86
PRATYUSH KUMAR <pratyush1002@gmail.com>
2024-07-19 12:21:35 +0530

    Additional Necessary Dependency Added
    Map Struct Added and Java Client added in POM

--- src/main/java/com/pratyush/springcouchbase/SpringBootWithCouchbaseApplication.java
+++ src/main/java/com/pratyush/springcouchbase/SpringBootWithCouchbaseApplication.java
@@ -3,11 +3,16 @@ package com.pratyush.springcouchbase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class SpringBootWithCouchbaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithCouchbaseApplication.class, args);
+		log.info("--Application Strated Successfully---");
+		System.err.println("From Main Method : --Application Strated Successfully---");
 	}
 
 }
